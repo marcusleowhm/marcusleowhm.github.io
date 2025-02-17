a665d4438de103717c624faa3860a9819bd2f90b
Marcus LEOW <leowmarcus13@hotmail.com>
2025-02-17 20:36:04 +0800

    [TECH] feat: install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          white: {
+            DEFAULT: "#F4F4F4"
+          },
+        },
+        fontFamily: {
+          sans: ["Inter", "sans-serif"],
+        },
+        fontSize: {
+          h1: ["3rem", "4rem"],
+          h2: ["2.5rem", "3.5rem"],
+          h3: ["2rem", "2.75rem"],
+          h4: ["1.5rem", "2rem"],
+          h5: ["1rem", "1.4rem"],
+          body: ["0.875rem", "1.25rem"],
+          finePrint: ["0.75rem", "1rem"],
+          tiny: ["0.6875rem", "0.9375rem"],
+        },
+        gridTemplateColumns: {
+          16: "grid-template-columns: repeat(16, minmax(0, 1fr));",
+        },
+        screens: {
+          md: "780px",
+          lg: "1028px",
+          xl: "1280px",
+        },
+        transitionProperty: {
+          "max-height": "max-height"
+        }
+      },
+    },
+    plugins: [],
+  }

62d1966a34d42c5641599a79b7fbdf332b742ea8
Marcus LEOW <leowmarcus13@hotmail.com>
2025-02-22 22:48:29 +0800

    [TECH][marcus] feat: add dynamic title depending on setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "@/assets/styles/main.scss";
+import React from "react";
+import config from "../config/profile.json";
 import { Hero } from "./components/Hero";
 import { Tab } from "./components/TabContainer";
 
 function App() {
+  React.useEffect(() => {
+    document.title = `${config.name}'s Profile`
+  },[])
+
   return (
     <>
       <div className="flex flex-col h-full">

ec89d2a171a6eaf8d31bd26b09abc264ec5950e9
Marcus LEOW <leowmarcus13@hotmail.com>
2025-05-05 18:03:29 +0800

    [TECH][marcus] feat: added drawer that can open and close

--- src/components/layout/BasePage.tsx
+++ src/components/layout/BasePage.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { GridLayout } from '@/components/layout/GridLayout';
 import { Navbar } from '@/components/nav/Navbar';
 import { Button } from '@/components/Button';
+import { Drawer } from '../nav/Drawer';
 
 export const BasePage = ({ children }: React.ComponentPropsWithoutRef<"div">): React.JSX.Element => {
+
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState<boolean>(false);
+
   return (
     <>
       <GridLayout id="nav__container">
         <Navbar className="md:block col-start-2 col-span-10 hidden"/>
-        <Button className="md:hidden" variant='contained'>Open Menu</Button>
+        <Button className="md:hidden" variant='contained' onClick={() => setIsDrawerOpen(true)}>Open Menu</Button>
       </GridLayout>
+      {isDrawerOpen && <Drawer setIsDrawerOpenToClose={() => setIsDrawerOpen(false)}/>}
       <GridLayout id="page__content" className="flex-1">
         <div className="md:col-start-2 col-span-10 p-4">
           {children}
---
+++ src/components/nav/Drawer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from '../Button'
+import { CloseSvg } from '../svg/CloseSvg'
+
+interface DrawerProps extends React.ComponentPropsWithRef<"div"> {
+    setIsDrawerOpenToClose: () => void
+}
+
+export const Drawer = ({ className, setIsDrawerOpenToClose }: DrawerProps): React.JSX.Element => {
+    return (
+        <div className={`fixed top-0 left-0 z-40 p-2 overflow-y-auto transition-transform bg-white dark:bg-gray-800 w-80 h-screen ${className}`.trim()}>
+            <Button variant="text" onClick={setIsDrawerOpenToClose}>
+                <CloseSvg />
+                <span className="sr-only">Close menu</span>
+            </Button>
+        </div>
+    )
+}

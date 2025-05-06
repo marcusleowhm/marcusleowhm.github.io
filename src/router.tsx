461cd48659f68d6cc0d0198e28627c82809a2768
Marcus LEOW <leowmarcus13@hotmail.com>
2025-05-06 20:12:07 +0800

    [TECH][marcus] feat: change to hash router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { HomePage } from "@/components/pages/HomePage";
 import { ExperiencePage } from "@/components/pages/ExperiencePage";
 import { ProjectPage } from "@/components/pages/ProjectPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <HomePage />

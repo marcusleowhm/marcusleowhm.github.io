88a9374c31a628b6fc525b31a7864a4561cd0283
Marcus LEOW <leowmarcus13@hotmail.com>
2025-05-11 04:04:34 +0800

    [TECH][marcus] feat: install react-markdown, vite-plugin-markdown and remark-gfm

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Mode, plugin } from "vite-plugin-markdown";
 import path from "path";
 
 // https://vite.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src")
     }
   },
-  plugins: [react()],
+  plugins: [react(), plugin({ mode: [Mode.MARKDOWN]})],
   css: {
     preprocessorOptions: {
       scss: {

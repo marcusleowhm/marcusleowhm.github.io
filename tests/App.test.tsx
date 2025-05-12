ea16312f363a5500289c0260f345e224b9313a4b
Marcus LEOW <leowmarcus13@hotmail.com>
2025-05-12 15:12:53 +0800

    [TECH][marcus] feat: add test path to tsconfig.app.json to resolve src in test files

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,9 +1,13 @@
-import { describe, it } from "vitest";
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from "vitest";
+import { render, waitFor } from '@testing-library/react';
 import { App } from "@/App";
 
 describe('App', () => {
-    it('should render App component', () => {
+    it("should render App component with index page's title", async () => {        
+        const expectedTitle = "- About me";
+        
         render(<App />)
-    }) 
+        
+        await waitFor(() => expect(document.title).toBe(expectedTitle))
+    })
 })
